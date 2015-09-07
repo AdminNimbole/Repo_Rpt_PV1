b3fafe917eeadaa9e69d310601e02e09a89d1a72
mujahed85 <mujahed.mca@gmail.com>
2015-09-07 20:22:01 +0530

    Added Default Page Startup

--- PV_RPT/App_Start/RouteConfig.cs
+++ PV_RPT/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PV_RPT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
